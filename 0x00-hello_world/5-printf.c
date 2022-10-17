b47ae6f9e78a7c92b4f20a28a6cef49006448847
MohmmdFayez <Mohmmedfaiz19@gmail.com>
2022-10-16 17:11:18 -0700

    c helloworld

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * description - startprogram
+ * Return: Alwayes 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
