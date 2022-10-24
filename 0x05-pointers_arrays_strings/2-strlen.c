3d497604fdd376a326d61bc5e0871b502fce50d9
MohmmdFayez <Mohmmedfaiz19@gmail.com>
2022-10-24 14:48:30 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -15,38 +15,4 @@ int _strlen(char *s)
 		s++;
 	}
 	return (i);
-}#include "main.h"
-/**
- * _strlen - calculates the length of a string.
- * @s: pointer to the character array.
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}#include "main.h"
-/**
- * _strlen - calculates the length of a string.
- * @s: pointer to the character array.
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
 }
